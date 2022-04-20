6a89938a91aa1039a287aac76cbfd54270d29259
dorijank <dorijank@localhost.localdomain>
2022-04-21 00:21:28 +0200

    Added semi functional prompt

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 
-int main(int argc, int **argv){
+int main(int argc, char *argv[]){
     char *input=malloc(sizeof(int));
-    while (1>0)
-    {
-        printf("prompt$");
-        scanf("%c",&input);
-        printf("\n");
-    }
     
-
+    for(;;){
+        printf("prompt$");
+        scanf("%s",input);
+    }
+    free(input);
     return 0;
 }
