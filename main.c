e818e11482896d4925792c7c0bd820d695a1218c
DorijanKom <dorijan.komsic@stu.ibu.edu.ba>
2022-04-30 14:50:15 +0200

    Added hostname and username to prompt but working

--- main.c
+++ main.c
@@ -2,17 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h>
 
 
+void prompt(){
+    
+    char *username[LOGIN_NAME_MAX];
+    char hostname[HOST_NAME_MAX];
+    getlogin_r(*username,LOGIN_NAME_MAX);
+    gethostname(hostname,HOST_NAME_MAX);
 
+    printf("%s@%s>",hostname,username);
+}
 
 
 int main(void){
 
+    // We use the input variable for user input
     char input[1024];
+    
     while (1)
     {
-        printf("prompt$");
+        prompt();
+        /* fgets is more optimal than scanf for user input because it reads a line of text and is
+        better at handling overflow of arrays */
         fgets(input,1024,stdin);
     }
         
